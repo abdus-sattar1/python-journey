2d47f28c7fa27088239971ca16db0a1fcddc2df6
abdus-sattar1 <114483932+abdus-sattar1@users.noreply.github.com>
2026-03-15 15:48:01 +0000

    added f-strings

--- basics.py
+++ basics.py
@@ -1,7 +1,22 @@
+# use terminal to run the file, by typing python3 followed by file name
+
 print("hello world and welcome to my journey!")
+
+# i'm going to make everything number related!
 
 number_chosen = int(input("think of a positive number "))
 if number_chosen > 0:
-    print(f"{number_chosen} is a positive number)")
+    print(f"{number_chosen} is a positive number")
 else: print("that is not a positive number!")
+
+
+user_name = input("Enter name: ")
+user_age = input("Enter age: ")
+
+# we can use "" or '' to print a string of letters
+
+print("User's name is " + user_name + " and they are " + user_age + " years old")
+print(f"User's name is {user_name} and they are {user_age} years old!")
+
+# second way is quicker
 
