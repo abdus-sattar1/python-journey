3471cbc149247be1e940fdb2c5b30b62f8e7fbed
abdus-sattar1 <114483932+abdus-sattar1@users.noreply.github.com>
2026-03-15 12:24:43 +0000

    new update

--- basics.py
+++ basics.py
@@ -1,1 +1,7 @@
-print("hello world!")
+print("hello world and welcome to my journey!")
+
+number_chosen = int(input("think of a positive number "))
+if number_chosen > 0:
+    print(f"{number_chosen} is a positive number)")
+else: print("that is not a positive number!")
+
